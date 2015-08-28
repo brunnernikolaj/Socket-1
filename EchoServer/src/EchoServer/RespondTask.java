ff7d4b33ff24e1654cf715570d1c313e12742b95
brunnernikolaj <brunnernikolaj@gmail.com>
2015-08-28 13:41:17 +0200

    Update RespondTask.java

--- EchoServer/src/EchoServer/RespondTask.java
+++ EchoServer/src/EchoServer/RespondTask.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package EchoServer;
 
 import java.io.BufferedReader;
@@ -19,6 +15,7 @@ import java.util.logging.Logger;
 /**
  *
  * @author Nikolaj
+ * This Class takes care of a connected client, and responds to all clients when writen to 
  */
 public class RespondTask implements Runnable {
 
@@ -37,28 +34,29 @@ public class RespondTask implements Runnable {
         try {
             InputStreamReader isr = new InputStreamReader(socket.getInputStream());
             BufferedReader br = new BufferedReader(isr);
-
+            
+            //Read input form client, until close is read
             String input;
             while (!(input = br.readLine()).equals("close")) {
                 try {
-
+                    //Apply action to input, Uppercase the specified action so its case-insensitive 
                     String[] action = input.split("#");
                     String returnVal = actions.get(action[0].toUpperCase()).apply(action[1]);
 
+                    //Write to all clients
                     for (Socket soc : clients) {
                         PrintStream output = new PrintStream(soc.getOutputStream());
                         output.printf("Client %d: %s \n", clients.indexOf(socket) + 1, returnVal);
                     }
                 } catch (Exception ex)  {
-                    
                     break;
                 }
             }
             System.out.printf("Client %d: Disconnected \n", clients.indexOf(socket) + 1);
+            //Remove client from Clients
             clients.remove(clients.indexOf(socket));
         } catch (IOException ex) {
             Logger.getLogger(RespondTask.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
 }
